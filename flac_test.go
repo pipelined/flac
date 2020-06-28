6d5e9c09d32ff405a55f9d829b918c59eff6495d
Sergei Dudka <serg.dudk@gmail.com>
2020-06-28 15:43:49 +0200

    remove wav dependency

--- flac_test.go
+++ flac_test.go
@@ -9,8 +9,6 @@ import (
 	"pipelined.dev/flac"
 	"pipelined.dev/pipe"
 	"pipelined.dev/pipe/mock"
-	"pipelined.dev/signal"
-	"pipelined.dev/wav"
 )
 
 const (
@@ -18,29 +16,22 @@ const (
 	bufferSize = 322
 	samples    = 330528
 	inputFile  = "_testdata/sample.flac"
-	outputFile = "_testdata/out.wav"
 )
 
 func TestFlacPipe(t *testing.T) {
 	in, _ := os.Open(inputFile)
-	outFile, _ := os.Create(outputFile)
 
 	source := flac.Source{Reader: in}
-	processor := &mock.Processor{}
-	sink := wav.Sink{
-		WriteSeeker: outFile,
-		BitDepth:    signal.BitDepth16,
-	}
+	sink := mock.Sink{}
 	line, err := pipe.Routing{
-		Source:     source.Source(),
-		Processors: pipe.Processors(processor.Processor()),
-		Sink:       sink.Sink(),
+		Source: source.Source(),
+		Sink:   sink.Sink(),
 	}.Line(bufferSize)
 
 	p := pipe.New(context.Background(), pipe.WithLines(line))
 	err = p.Wait()
 	assertEqual(t, "wait err", err, nil)
-	assertEqual(t, "samples", processor.Samples, samples)
+	assertEqual(t, "samples", sink.Samples, samples)
 
 	err = in.Close()
 	assertEqual(t, "close err", err, nil)
