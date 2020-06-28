86440eb20e74246dbac06b36f6d891ffc6833fed
Sergei Dudka <serg.dudk@gmail.com>
2020-06-28 15:40:39 +0200

    simplify code structure

--- flac.go
+++ flac.go
@@ -12,67 +12,52 @@ import (
 	"pipelined.dev/signal"
 )
 
-// Source reads flac data from ReadSeeker.
+// Source decodes PCM from flac data io.Reader.
 type Source struct {
 	io.Reader
 	decoder *flac.Stream
-	frame   *frame.Frame
-	pos     int
+	state   *readState
 	ints    signal.Signed
 }
 
-// Source starts the pump stage.
+// Source returns new Source allocator function.
 func (s Source) Source() pipe.SourceAllocatorFunc {
 	return s.allocator
 }
 
-func (s *Source) allocator(bufferSize int) (pipe.Source, pipe.SignalProperties, error) {
+func (s Source) allocator(bufferSize int) (pipe.Source, pipe.SignalProperties, error) {
 	decoder, err := flac.New(s.Reader)
 	if err != nil {
 		return pipe.Source{}, pipe.SignalProperties{}, fmt.Errorf("error creating FLAC decoder: %w", err)
 	}
 	s.decoder = decoder
-	channels := int(decoder.Info.NChannels)
 	s.ints = signal.Allocator{
-		Channels: channels,
+		Channels: int(decoder.Info.NChannels),
 		Length:   bufferSize,
 		Capacity: bufferSize,
 	}.Int32(signal.BitDepth(decoder.Info.BitsPerSample))
+	s.state = &readState{}
 	return pipe.Source{
 			SourceFunc: s.source,
 			FlushFunc:  s.flush,
 		},
 		pipe.SignalProperties{
 			SampleRate: signal.SampleRate(decoder.Info.SampleRate),
-			Channels:   channels,
+			Channels:   int(decoder.Info.NChannels),
 		}, nil
 }
 
-func (s *Source) source(floats signal.Floating) (read int, err error) {
-outer:
-	for {
-		if s.frame == nil {
-			if s.frame, err = s.decoder.ParseNext(); err != nil {
-				if err == io.EOF {
-					break // no more bytes available
-				} else {
-					return 0, fmt.Errorf("error reading FLAC frame: %w", err)
-				}
+func (s Source) source(floats signal.Floating) (int, error) {
+	read := 0
+	for read < s.ints.Len() {
+		if err := s.state.nextFrame(s.decoder); err != nil {
+			if err == io.EOF {
+				break
 			}
+			return 0, fmt.Errorf("error reading FLAC frame: %w", err)
 		}
 
-		for s.pos < int(s.frame.BlockSize) {
-			if read == s.ints.Len() {
-				break outer
-			}
-			for sf := range s.frame.Subframes {
-				s.ints.SetSample(read, int64(s.frame.Subframes[sf].Samples[s.pos]))
-				read++
-			}
-			s.pos++
-		}
-		s.frame = nil
-		s.pos = 0
+		read = s.state.readFrame(s.ints, read)
 	}
 	if read == 0 {
 		return 0, io.EOF
@@ -84,6 +69,37 @@ outer:
 }
 
 // Flush closes flac decoder.
-func (s *Source) flush(context.Context) error {
+func (s Source) flush(context.Context) error {
 	return s.decoder.Close()
+}
+
+type readState struct {
+	*frame.Frame
+	pos int
+}
+
+func (state *readState) nextFrame(stream *flac.Stream) error {
+	if state.Frame == nil || state.pos == int(state.Frame.BlockSize) {
+		f, err := stream.ParseNext()
+		if err != nil {
+			return err
+		}
+		state.Frame = f
+		state.pos = 0
+	}
+	return nil
+}
+
+func (state *readState) readFrame(ints signal.Signed, read int) int {
+	for state.pos < int(state.Frame.BlockSize) {
+		if read == ints.Len() {
+			break
+		}
+		for sf := range state.Frame.Subframes {
+			ints.SetSample(read, int64(state.Frame.Subframes[sf].Samples[state.pos]))
+			read++
+		}
+		state.pos++
+	}
+	return read
 }
